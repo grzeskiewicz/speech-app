fd2df609cf236d6c3b35b4f2d96213d436bd7306
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-11-04 20:55:11 +0100

    anotther update

--- src/index.js
+++ src/index.js
@@ -29,6 +29,165 @@ recognition.interimResults = true;
 recognition.start();
 
 
+
+
+class PreviousMonth extends React.Component {
+    render() {
+        return (
+            <span id="previous" onClick = {(nextClicked) => this.props.onClick(nextClicked)}> &lt;&lt; </span>
+        )
+    }
+}
+
+
+class NextMonth extends React.Component {
+    render() {
+        return (
+            <span id="next" onClick = {(nextClicked) => this.props.onClick(nextClicked)}> &gt;&gt; </span>
+        )
+    }
+}
+
+
+class Calendar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            yearNow: new Date().getFullYear(),
+            selectedMonth: new Date().getMonth(),
+            showNext: false
+        };
+
+    }
+
+    createCalendar(year, month) {
+        const results = [];
+
+        // find out first and last days of the month
+        const firstDate = new Date(year, month, 1); //first day of the month
+        const lastDate = new Date(year, month + 1, 0); //last day of month
+        // calculate first monday and last sunday
+        const firstMonday = this.getFirstMonday(firstDate);
+        const lastSunday = this.getLastSunday(lastDate);
+
+        // iterate days starting from first monday
+        let iterator = new Date(firstMonday);
+        let i = 0;
+
+        // ..until last sunday
+        while (iterator <= lastSunday) {
+            if (i++ % 7 === 0) {
+                // start new week when monday
+                var week = [];
+                results.push(week);
+            }
+            // push day to week
+            week.push({
+                date: new Date(iterator),
+                before: iterator < firstDate, // add indicator if before current month
+                after: iterator > lastDate // add indicator if after current month
+            });
+            // iterate to next day
+            iterator.setDate(iterator.getDate() + 1);
+        }
+        return results;
+    }
+
+    fixMonday(day) {
+        day || (day = 7);
+        return --day;
+    }
+
+    getFirstMonday(firstDate) { //first monday closest to 1st day of mondth
+        const offset = this.fixMonday(firstDate.getDay()); //how many days from 1st to monday
+
+        const result = new Date(firstDate);
+        result.setDate(firstDate.getDate() - offset); //create first monday : 1st day of the month - offset
+        return result;
+    }
+
+    getLastSunday(lastDate) {
+        const offset = 6 - this.fixMonday(lastDate.getDay()); //how many days till monday (6-dayOfTheWeek -1)
+
+        const result = new Date(lastDate);
+        result.setDate(lastDate.getDate() + offset); //last possible sunday after last day of the month
+
+        return result;
+    }
+
+
+    changeMonth(nextClicked) {
+        const monthNow = new Date().getMonth();
+        let showNext = this.state.showNext;
+        let selectedMonth = this.state.selectedMonth;
+        nextClicked ? selectedMonth++ : selectedMonth--;
+
+        showNext = (selectedMonth < monthNow) ? true : false;
+
+        this.setState({
+            selectedMonth: selectedMonth,
+            showNext: showNext,
+        })
+    }
+
+
+    render() {
+        const today = new Date();
+        const calendar = this.createCalendar(this.state.yearNow, this.state.selectedMonth);
+        const DAY_NAMES = 'Mo Tu We Th Fr Sa Su'.split(' ');
+        const MONTH_NAMES = 'January February March April May June July August September October November December'.split(' ');
+
+
+        const daysOfTheWeek = DAY_NAMES.map((day, index) => {
+            return (
+                <td key={index}>{day}</td>
+            );
+        });
+
+
+        const weeksToRender = [];
+        for (let j = 0; j < calendar.length; j++) {
+            const week = calendar[j];
+
+            const weekMap = week.map((day, index) => {
+                let classes = '';
+                if (day.date.getMonth() === today.getMonth() && day.date.getDate() === today.getDate()) classes = classes.concat('today ');
+                if (day.date.getDate() > today.getDate() && day.date.getMonth() === today.getMonth()) classes = classes.concat('not-selectable ');
+                if (day.date.getMonth() > today.getMonth()) classes = classes.concat('not-selectable ');
+                if (day.before) classes = classes.concat('before ');
+                if (day.after) classes = classes.concat('after ');
+
+                return (
+                    <td data-date={day.date} key={index} className={classes} onClick={()=>this.props.onClick(day.date)}>{day.date.getDate()}</td>
+                );
+            });
+            weeksToRender.push(weekMap);
+
+        }
+
+        const month = weeksToRender.map((week, index) => {
+            return (
+                <tr key={index}>{week}</tr>
+            );
+        });
+
+
+        return (
+            <div>
+            <div id="month-control">
+             <PreviousMonth onClick={() => this.changeMonth(false)} />  
+             {MONTH_NAMES[this.state.selectedMonth]} 
+             {this.state.showNext ? <NextMonth onClick={() => this.changeMonth(true)} /> : null } 
+             </div>
+                <table>
+                    <thead><tr>{daysOfTheWeek}</tr></thead>
+                    <tbody>{month}</tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
 class Board extends React.Component {
     constructor(props) {
         super(props);
@@ -62,7 +221,7 @@ class Board extends React.Component {
         const importedNotes = (window.localStorage.getItem('notes')) === null ? 'No notes in the storage' : window.localStorage.getItem('notes');
         //console.log(importedNotes);
         this.setState({
-            importedNotes: importedNotes
+            importedNotes: importedNotes.split(',')
         });
         //console.log(importedNotes);
 
@@ -78,8 +237,9 @@ class Board extends React.Component {
 
 
     saveToDb() {
-        const notes = { content: JSON.stringify(this.state.notes) };
-        // const notes = { 'content': "Hehehehxxxxx" };
+        console.log(this.state.importedNotes);
+        console.log(this.state.notes);
+        const notes = { content: JSON.stringify(this.state.notes.concat(this.state.importedNotes)) };
         fetch(request(API_URL + "savenotes", 'POST', notes))
             .then(res => res.json())
             .then(result => {
@@ -97,10 +257,12 @@ class Board extends React.Component {
 
 
     importFromDbDate(datePicked) {
-        const date= {date: datePicked }
+        console.log(datePicked);
+        const date = { date: datePicked }
         fetch(request(API_URL + "getnotesdate", 'POST', date))
             .then(res => res.json())
             .then(result => {
+                console.log(result);
                 this.setState({ importedDbNotes: result });
             });
     }
@@ -118,6 +280,12 @@ class Board extends React.Component {
                 <li key={index}>{note}</li>
             );
         });
+        const importedDbNotes = this.state.importedDbNotes.map((note, index) => {
+            return (
+                <li key={index}>{note.content}</li>
+            );
+        });
+
 
         return (
             <div>
@@ -127,9 +295,15 @@ class Board extends React.Component {
              <button onClick = {() => this.saveStorage()}> Save notes to storage </button>
             <button onClick = {() => this.importStorage()}> Import notes from storage </button>
 <button onClick = {() => this.removeStorage()}> Remove storage </button>
-
 <button onClick = {() => this.saveToDb()}> Save notes to DB </button>
-<button onClick = {() => this.importFromDb()}> Import notes from DB </button>
+<div id="day-notes">
+<Calendar onClick={(datePicked) => this.importFromDbDate(datePicked)}/>
+<div>
+<ol>
+    {importedDbNotes}
+</ol>
+</div>
+</div>
             </div>
 
 
